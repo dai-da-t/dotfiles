dc57ba2a34899332bd99ea17a2b51bc8569b4b59
tiida <tiida@keio.jp>
2021-07-08 18:48:05 +0900

    [change] CUDA Version 10.0 -> 10.1

--- zsh/01_linux.zsh
+++ zsh/01_linux.zsh
@@ -3,6 +3,6 @@ export PATH=$PYENV_ROOT/bin:$PATH
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
-export CUDA_HOME=/usr/local/cuda-10.0
+export CUDA_HOME=/usr/local/cuda-10.1
 export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
